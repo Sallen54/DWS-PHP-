41d56b7503d9bb4c1bf6bf8c311106d2361dc819
Alejandro <alejandalmau@gmail.com>
2025-11-21 15:43:04 +0100

    Correcion

--- Activitades PHP/Sprint 3/PDO/Conexion.class.php
+++ Activitades PHP/Sprint 3/PDO/Conexion.class.php
@@ -21,11 +21,11 @@ class Conexion
         $user     = "tendaFake";
         $password = "Lliurex_01";
         try {
-            $pdo = new PDO("mysql:host=$host; dbname=$dbname;charset=utf8;$user,$password");
+            $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $pdo;
         } catch (PDOException $e) {
-            echo "Error de conexion" . $e->getMessage();
+            echo "Error de conexion: " . $e->getMessage();
             return null;
         }
     }
